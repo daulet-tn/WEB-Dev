0cdf6446704bebe6bd54509d33cabc0ef2bb0b81
daulet_tn <daulettanirberdi94@gmail.com>
2026-02-03 12:50:34 +0500

    1

--- for_another/lab2/task2/Main.java
+++ for_another/lab2/task2/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        Triangle item = new Triangle(4);
+        Triangle item = new Triangle(10);
         System.out.println(item.Starttriangle());
     }
 }
